c068d0bdfbb6918d1a21bcd751e6bafc23e5d04d
mrashutoshmuduli <140726864+mrashutoshmuduli@users.noreply.github.com>
2025-03-31 21:58:50 +0530

    DB config changes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,19 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'mysql-server',
-  user: 'root',
-  password: 'Welcome@123',
-  database: 'testdb',
-  authPlugins: {
-    mysql_clear_password: () => () => Buffer.from('Welcome@123') // Ensure this matches the actual password
+  host: "mysql-server",
+  user: "root",
+  password: "Welcome@123",
+  database: "testdb"
+});
+
+// Connect to MySQL
+connection.connect(err => {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
   }
+  console.log("Connected to MySQL database");
 });
 
 module.exports = connection;
